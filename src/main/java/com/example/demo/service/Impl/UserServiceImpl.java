d48f194f64afd60e1da5634a64cda95ba6ba9bd0
eggegg <njingdan@163.com>
2025-09-22 18:26:22 +0800

    initial commit

---
+++ src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.demo.service.Impl;
+
+import com.example.demo.entity.User;
+import com.example.demo.mapper.UserMapper;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public List<User> list() {
+        return userMapper.list();
+    }
+}
---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,9 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+    public List<User> list();
+}
